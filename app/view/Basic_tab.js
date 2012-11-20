b311361502de3a18eb85acdf1baf47aa2d6be4a0
ylh6151 <ylh6151@gmail.com>
2012-11-20 22:00:27 +0900

    tab

--- app/view/Basic_tab.js
+++ app/view/Basic_tab.js
@@ -1,35 +1,56 @@
 Ext.define('team3.view.Basic_tab', {
-		extend: 'Ext.Panel',
-		xtype: 'Basic_tab',
-		
-		
-		config: {
-			//title: '이전단계',
-			iconCls: 'arrow_left',
-			//cls: 'home',
-			//iconMask : true,
-			iconAlign : 'left',
-			
-			scrollable: true,
-			styleHtmlContent: true,
-			
-			items: {
-				docked : 'top',
-				xtype : 'titlebar',
-				title: 'Tab'
-			},
-			
-	        html: [
-	           '<img src="http://staging.sencha.com/img/sencha.png" />',
-	        ].join(""),
-	        
-	        listeners: {
-	        	touchstart: {
-					fn:function(event) {
-						console.log("touchstart:" + event.touch.pageX + "," + event.touch.pageY);
-					},
-					element: "element"						
-				},
-	        }
-		}
-	});
+    extend: 'Ext.Panel',
+    xtype: 'Basic_tab',
+    
+    config: {
+    	title: 'Blog',
+        iconCls: 'star',
+        
+    	items: [
+            {
+                xtype: 'toolbar',
+                docked: 'top',
+                title: 'Tab'
+            },
+            {
+                xtype: 'toolbar',
+                docked: 'bottom',
+                items: [
+                    {
+                        xtype: 'spacer',
+                        ui: ''
+                    },
+                    {
+                        xtype: 'button',
+                        iconCls: 'arrow_left',
+                        iconMask: true,
+                        text: '이전'
+                    },
+                    {
+                        xtype: 'button',
+                        iconCls: 'home',
+                        iconMask: true,
+                        text: ''
+                    },
+                    {
+                        xtype: 'button',
+                        iconCls: 'refresh',
+                        iconMask: true,
+                        text: ''
+                    },
+                    {
+                        xtype: 'button',
+                        iconAlign: 'right',
+                        iconCls: 'arrow_right',
+                        iconMask: true,
+                        text: '다음'
+                    },
+                    {
+                        xtype: 'spacer'
+                    }
+                ]
+            }
+        ]
+    }
+
+});
